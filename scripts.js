b5f70b37c7906b7504bb70f90d9444f73f2b9199
Nufina <mailoffina@gmail.com>
2018-10-23 20:51:10 +0700

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,55 @@
+var slideIndex = 0;
+var i = 0;
+var slides, dots;
+
+function plusSlides(position) {
+    "use strict";
+    slideIndex += position;
+    if (slideIndex > slides.length) {slideIndex = 1; }
+    
+    else if (slideIndex < 1) {slideIndex = slides.length; }
+    
+    for (i = 0; i < slides.length; i++) {
+       slides[i].style.display = "none";  
+    }
+    
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+
+    }
+        slides[slideIndex-1].style.display = "block";  
+        dots[slideIndex-1].className += " active";
+}
+
+function currentSlide(index) {
+    if (index > slides.length) {index = 1}
+    else if(index < 1){index = slides.length}
+    for (i = 0; i < slides.length; i++) {
+       slides[i].style.display = "none";  
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+      }
+        slides[index-1].style.display = "block";  
+        dots[index-1].className += " active";
+}
+
+function showSlides() {
+    "use strict";
+    var i;
+    slides = document.getElementsByClassName("slide");
+    dots = document.getElementsByClassName("dot");
+    for (i = 0; i < slides.length; i++) {
+       slides[i].style.display = "none";  
+    }
+    slideIndex++;
+    if (slideIndex> slides.length) {slideIndex = 1}    
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex-1].style.display = "block";  
+    dots[slideIndex-1].className += " active";
+    setTimeout(showSlides, 6000); // Change image every 3 seconds
+}
+
+showSlides();
